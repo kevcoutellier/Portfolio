fce59f508700d356d260ee17f337091c1eb50381
kevincoutellier <kevin.coutellier@epitech.eu>
2025-09-17 17:20:41 +0200

    Ajout des Images de preview project

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,22 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   // Remove turbo config as it's causing font resolution issues
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'shot.screenshotapi.net',
+      },
+      {
+        protocol: 'https',
+        hostname: 'via.placeholder.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+      }
+    ],
+  },
 };
 
 export default nextConfig;
